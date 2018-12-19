51b2d23561e41a0a0b5e8a8574031d4238604d42
sukhmen <sukhmennijjar@gmail.com>
2018-12-19 01:36:33 -0800

    Final

--- js/main.js
+++ js/main.js
@@ -74,118 +74,118 @@ $(document).ready(function(){
 	$(".img-popup").on('click',function(){
 	   var src = $(".img-popup").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup1").on('click',function(){
 	   var src = $(".img-popup1").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup2").on('click',function(){
 	   var src = $(".img-popup2").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup3").on('click',function(){
 	   var src = $(".img-popup3").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup4").on('click',function(){
 	   var src = $(".img-popup4").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup5").on('click',function(){
 	   var src = $(".img-popup5").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup6").on('click',function(){
 	   var src = $(".img-popup6").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup7").on('click',function(){
 	   var src = $(".img-popup7").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup8").on('click',function(){
 	   var src = $(".img-popup8").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup9").on('click',function(){
 	   var src = $(".img-popup9").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup10").on('click',function(){
 	   var src = $(".img-popup10").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup11").on('click',function(){
 	   var src = $(".img-popup11").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup12").on('click',function(){
 	   var src = $(".img-popup12").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup13").on('click',function(){
 	   var src = $(".img-popup13").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup14").on('click',function(){
 	   var src = $(".img-popup14").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup15").on('click',function(){
 	   var src = $(".img-popup15").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
 $(document).ready(function(){
 	$(".img-popup16").on('click',function(){
 	   var src = $(".img-popup16").attr('src');
 	   $("#modalImg").attr('src',src);
-	   $(".caption").text('Hello');
+	   $(".caption").text('');
 	});
 });
